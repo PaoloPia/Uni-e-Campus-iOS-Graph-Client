16fc3598df20d0b3f6f038189c8a793f4f63f35e
Paolo Pialorsi <paolopialorsi@Paolos-Air.lan>
2021-11-14 19:21:43 +0100

    Initial commit of iOS app

---
+++ src/Uni-e-Campus-iOS-Graph-Client/Pods/Target Support Files/Pods-Uni-e-Campus-iOS-Graph-Client/Pods-Uni-e-Campus-iOS-Graph-Client-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Uni_e_Campus_iOS_Graph_Client : NSObject
+@end
+@implementation PodsDummy_Pods_Uni_e_Campus_iOS_Graph_Client
+@end
---
+++ src/Uni-e-Campus-iOS-Graph-Client/Uni-e-Campus-iOS-Graph-Client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Uni-e-Campus-iOS-Graph-Client
+//
+//  Created by Paolo Pialorsi on 14/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
