d759acbddef45237a737cdbb343cc8583a226d9a
Paolo Pialorsi <paolopialorsi@Paolos-Air.lan>
2021-11-16 10:15:12 +0100

    Code complete, missing calendar invocation

--- src/Uni-e-Campus-iOS-Graph-Client/Uni-e-Campus-iOS-Graph-Client/AppDelegate.swift
+++ src/Uni-e-Campus-iOS-Graph-Client/Uni-e-Campus-iOS-Graph-Client/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MSAL
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -31,6 +32,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    // Inside AppDelegate...
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
 
+            return MSALPublicClientApplication.handleMSALResponse(url, sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String)
+    }
 }
 
